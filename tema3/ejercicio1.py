71576e5077ac963cd96e832a8c0b7d21bff34260
diegoamengarelli <106921578+diegoamengarelli@users.noreply.github.com>
2022-07-24 08:25:55 -0400

    tema3

---
+++ tema3/ejercicio1.py
@@ -0,0 +1,25 @@
+# Asignacion de las variables
+
+nombre = "ALberto"
+apellidos = ("Perez", "Mena")
+edad = 35
+email = "albertoperez@test.com"
+telefono = 42526678
+casado = True
+con_hijos = False 
+lista_de_amigos = ["Juan CAballero", "Arnaldo Montes", "Anita Juarez"]
+peliculas_vistas = {
+    1 : "La doncella",
+    2 : "Minions",
+    3 : "Thor",
+}
+
+# Imprimir las variables
+print(nombre)
+print(apellidos)
+print(edad)
+print(telefono)
+print(casado)
+print(con_hijos)
+print(lista_de_amigos)
+print(peliculas_vistas)
