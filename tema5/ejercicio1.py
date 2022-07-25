b8ba5dc141f90b88ff3b24d0720a273cec1c582b
diegoamengarelli <106921578+diegoamengarelli@users.noreply.github.com>
2022-07-25 18:58:25 -0400

    5

---
+++ tema5/ejercicio1.py
@@ -0,0 +1,10 @@
+altura = int(input("inserte la altura del tringulo: "))
+base = int(input("inserte la base del tringulo: "))
+
+def area_triangulo(altura, base):
+    area = base * altura / 2
+    print(area)
+
+area_triangulo(altura, base)
+
+
