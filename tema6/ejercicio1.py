6193a368d40aa28f54083b9f21364a23bf5872fa
diegoamengarelli <106921578+diegoamengarelli@users.noreply.github.com>
2022-07-27 19:46:13 -0400

    Update ejercicio1.py

--- tema6/ejercicio1.py
+++ tema6/ejercicio1.py
@@ -1,11 +1,23 @@
-class calculadora:
-    def suma(a, b):
-        return a + b
-    def resta(a, b):
-        return a - b
-    def mult(a, b):
-        return a * b
-    def div(a, b):
-        return a / b
+class Vehiculo():
 
-print(calculadora.div(5, 8))
+    def __init__(self, color, ruedas, puertas):
+        self.color = color
+        self.ruedas = ruedas
+        self.puertas = puertas
+
+    def __str__(self):
+        return "Color {}, {} ruedas".format( self.color, self.ruedas, self.puertas )
+
+class Coche(Vehiculo):
+
+    def __init__(self, color, ruedas, puertas, velocidad, cilindrada):
+        Vehiculo.__init__(self, color, ruedas, puertas)
+        self.velocidad = velocidad
+        self.cilindrada = cilindrada
+
+    def __str__(self):
+        return "color {}, {} ruedas, {} puertas, {} cc, {} km/h".format( self.color, self.ruedas, self.puertas, self.cilindrada, self.velocidad)
+
+c = Coche("Azul", 4, 5, 120, 1500)
+
+print(c)
