33dc56fef03016663d04a853043d4261b95833f4
diegoamengarelli <106921578+diegoamengarelli@users.noreply.github.com>
2022-07-30 09:22:24 -0400

    7

---
+++ tema7/ejercicio1.py
@@ -0,0 +1,9 @@
+from calculadora import *
+
+a = 25
+b = 7
+
+print( "{} + {} = {}".format(a, b, suma(a, b) ) )
+print( "{} - {} = {}".format(a, b, resta(a, b) ) )
+print( "{} * {} = {}".format(a, b, multiplicar(a, b) ) )
+print( "{} / {} = {}".format(a, b, division(a, b) ) )
